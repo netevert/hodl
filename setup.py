63d8302ff135411c94e8237fe5f87168c977d017
ErrantBot <errantbot@tutanota.com>
2017-12-28 14:03:10 +0000

    committing v.1.0.0.dev1

--- setup.py
+++ setup.py
@@ -1,57 +1,103 @@
-from setuptools import setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+long_description = """
+Your friendly, no-nonsense script to instantaneously check cryptocurrency prices,
+helping you HODL one day at a time :)
+
+HODL's version is currently 1.0.0.dev1. It is currently under active development,
+with a production version expected to be released soon.
+"""
 
 setup(
     name='hodl',
     version='1.0.0.dev1',
-    packages=['hodl', 'venv.Lib.distutils', 'venv.Lib.encodings',
-              'venv.Lib.importlib', 'venv.Lib.collections',
-              'venv.Lib.site-packages.pip', 'venv.Lib.site-packages.pip.req',
-              'venv.Lib.site-packages.pip.vcs',
-              'venv.Lib.site-packages.pip.utils',
-              'venv.Lib.site-packages.pip.compat',
-              'venv.Lib.site-packages.pip.models',
-              'venv.Lib.site-packages.pip._vendor',
-              'venv.Lib.site-packages.pip._vendor.distlib',
-              'venv.Lib.site-packages.pip._vendor.distlib._backport',
-              'venv.Lib.site-packages.pip._vendor.colorama',
-              'venv.Lib.site-packages.pip._vendor.html5lib',
-              'venv.Lib.site-packages.pip._vendor.html5lib._trie',
-              'venv.Lib.site-packages.pip._vendor.html5lib.filters',
-              'venv.Lib.site-packages.pip._vendor.html5lib.treewalkers',
-              'venv.Lib.site-packages.pip._vendor.html5lib.treeadapters',
-              'venv.Lib.site-packages.pip._vendor.html5lib.treebuilders',
-              'venv.Lib.site-packages.pip._vendor.lockfile',
-              'venv.Lib.site-packages.pip._vendor.progress',
-              'venv.Lib.site-packages.pip._vendor.requests',
-              'venv.Lib.site-packages.pip._vendor.requests.packages',
-              'venv.Lib.site-packages.pip._vendor.requests.packages.chardet',
-              'venv.Lib.site-packages.pip._vendor.requests.packages.urllib3',
-              'venv.Lib.site-packages.pip._vendor.requests.packages.urllib3.util',
-              'venv.Lib.site-packages.pip._vendor.requests.packages.urllib3.contrib',
-              'venv.Lib.site-packages.pip._vendor.requests.packages.urllib3.packages',
-              'venv.Lib.site-packages.pip._vendor.requests.packages.urllib3.packages.ssl_match_hostname',
-              'venv.Lib.site-packages.pip._vendor.packaging',
-              'venv.Lib.site-packages.pip._vendor.cachecontrol',
-              'venv.Lib.site-packages.pip._vendor.cachecontrol.caches',
-              'venv.Lib.site-packages.pip._vendor.webencodings',
-              'venv.Lib.site-packages.pip._vendor.pkg_resources',
-              'venv.Lib.site-packages.pip.commands',
-              'venv.Lib.site-packages.pip.operations',
-              'venv.Lib.site-packages.wheel',
-              'venv.Lib.site-packages.wheel.test',
-              'venv.Lib.site-packages.wheel.test.simple.dist.simpledist',
-              'venv.Lib.site-packages.wheel.test.complex-dist.complexdist',
-              'venv.Lib.site-packages.wheel.tool',
-              'venv.Lib.site-packages.wheel.signatures',
-              'venv.Lib.site-packages._markerlib',
-              'venv.Lib.site-packages.setuptools',
-              'venv.Lib.site-packages.setuptools.command',
-              'venv.Lib.site-packages.pkg_resources',
-              'venv.Lib.site-packages.pkg_resources._vendor',
-              'venv.Lib.site-packages.pkg_resources._vendor.packaging'],
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'venv']),
+    # Required,
     url='https://github.com/errantbot/hodl',
     license='MIT',
     author='errantbot',
     author_email='errantbot@tutanota.com',
-    description='A  friendly, no-nonsense script to instantaneously check cryptocurrency prices.'
+    description='A  friendly, no-nonsense script to instantaneously check '
+                'cryptocurrency prices.',
+    # This is an optional longer description of your project that represents
+    # the body of text which users will see when they visit PyPI.
+    #
+    # Often, this is the same as your README, so you can just read it in from
+    # that file directly (as we have already done above)
+    #
+    # This field corresponds to the "Description" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#description-optional
+    long_description=long_description,  # Optional
+
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 2 - Pre-Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Other/Nonlisted Topic',
+        'Topic :: Terminals',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+
+        # project language
+        'Natural Language :: English',
+
+        # compatible os
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+    ],
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='cryptocurrencies exchange-rates coinbase command-line-tool ',
+    # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'hodl=hodl:main',
+        ], },
 )
