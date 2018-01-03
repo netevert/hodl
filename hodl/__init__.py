98879e28c9fdd52b2991c045208f46f7dd7e8aee
errantbot <errantbot@tutanota.com>
2018-01-03 23:49:09 +0100

    first commit

--- hodl/__init__.py
+++ hodl/__init__.py
@@ -8,8 +8,11 @@ except ImportError:
     import ConfigParser as cp
     from urllib2 import urlopen, Request, HTTPError
 import argparse
+from colorama import init, Fore
 import json
 import os
+
+init(autoreset=True)
 
 # constants
 description = "Your friendly, no-nonsense tool to instantaneously check cryptocurrency prices"
@@ -31,9 +34,9 @@ def get_price(crypto="BTC", fiat=config.get("currency", "FIAT")):
         req = Request(url)
         r = urlopen(req).read()
         data = json.loads(r.decode('utf-8'))
-        return "1 {} = {} {}".format(data['data']['base'],
-                                     data['data']['amount'],
-                                     data['data']['currency'])
+        return "1 {} = {} {} | ".format(data['data']['base'],
+                                        data['data']['amount'],
+                                        data['data']['currency'])
     except HTTPError:
         return "[*] error, check you are using correct crypto and fiat symbols"
 
@@ -55,6 +58,39 @@ def set_fiat(fiat):
         return "[*] error while configuring fiat, report: ", e
 
 
+def record_reading(base, amount):
+    """Saves the crypto-currency exchange value supplied to config.ini"""
+    try:
+        config.set("readings", base, amount)
+        with open(config_filename, 'w') as configfile:
+            config.write(configfile)
+    except Exception as e:
+        print(e)
+
+
+def print_report(report):
+    """Prints a crypto-currency exchnge rate report"""
+    if "[*] error, check you are using correct crypto and fiat symbols" in report:
+        print(report)
+    else:
+        base = report.split("=")[0].split(" ")[1]
+        current_amount = float(report.split("=")[1].split(" ")[1])
+        # recover cached record
+        previous_amount = float(config.get("readings", base.upper()))
+        if current_amount > previous_amount:
+            change = Fore.GREEN + "{0:.2f}% increase".format(100.0 * current_amount / previous_amount - 100)
+            print(report + change)
+            record_reading(base, str(current_amount))
+        elif current_amount < previous_amount:
+            change = Fore.RED + "{0:.2f}% decrease".format(100.0 * current_amount / previous_amount - 100)
+            print(report + change)
+            record_reading(base, str(current_amount))
+        elif current_amount == previous_amount:
+            change = Fore.YELLOW + "no change"
+            print(report + change)
+            record_reading(base, str(current_amount))
+
+
 def main():
     """Main program entry point; parses and interprets command line arguments"""
     parser = argparse.ArgumentParser(prog="HODL", description=description, epilog=epilog)
@@ -72,17 +108,17 @@ def main():
     args = parser.parse_args()
 
     if args.crypto and args.fiat:
-        print(get_price(args.crypto, args.fiat))
+        print_report(get_price(args.crypto, args.fiat))
     elif (args.set_fiat and args.crypto) or (args.crypto and args.set_fiat):
         print(set_fiat(args.set_fiat))
-        print(get_price(args.crypto))
+        print_report(get_price(args.crypto))
     elif args.crypto:
-        print(get_price(args.crypto))
+        print_report(get_price(args.crypto))
     elif args.set_fiat:
         print(set_fiat(args.set_fiat))
     else:
         for report in get_majors():
-            print(report)
+            print_report(report)
 
 
 if __name__ == '__main__':
